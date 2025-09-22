7040531195e81e595b5d08f1ee01acf82f5855b6
ciao-is-charlie <charlie.t0227@gmail.com>
2025-09-22 21:24:19 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode })' => {
     const env = loadEnv(mode, '.', '');
     return {
+      // highlight-next-line
+      base: '/Profile/', // <-- 新增您的 GitHub Repository 名稱
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
